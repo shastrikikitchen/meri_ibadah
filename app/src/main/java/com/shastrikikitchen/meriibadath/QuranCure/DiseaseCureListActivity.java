e55ab540ca428eef295d6adaeb722259583b2c51
Sauda sadaf <sauda.sadaf@snapdeal.com>
2016-08-21 22:07:43 +0530

    First Project case

---
+++ app/src/main/java/com/shastrikikitchen/meriibadath/QuranCure/DiseaseCureListActivity.java
@@ -0,0 +1,15 @@
+package com.shastrikikitchen.meriibadath.QuranCure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shastrikikitchen.meriibadath.R;
+
+public class DiseaseCureListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disease_cure_list);
+    }
+}
